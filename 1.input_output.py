d4ac65547a7c41ec7e81975d494fff8dc86311df
Bang <151416686+bunnyz8888@users.noreply.github.com>
2024-01-23 09:31:16 +0700

    Add files via upload

---
+++ 1.input_output.py
@@ -0,0 +1,7 @@
+#Tính và hiển thị ra màn hình tổng hai số nguyên bất kỳ
+print ("Nhap so thu nhat:")
+so1 = int(input())
+print ("Nhap so thu hai:")
+so2 = int(input())
+tong = so1 + so2
+print ("Tong hai so la", tong)
