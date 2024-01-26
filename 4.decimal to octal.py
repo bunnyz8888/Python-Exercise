6432691a7d0bc34dca721c73043381b96e34ffc9
Bang <151416686+bunnyz8888@users.noreply.github.com>
2024-01-26 09:15:37 +0700

    Add files via upload

---
+++ 4.decimal to octal.py
@@ -0,0 +1,4 @@
+#Nhập số bất kỳ ở hệ thập phân và hiển thị ra ở hệ bát phân
+print ("Nhap mot so thap phan")
+sothapphan = int(input())
+print ("So thap phan %d trong he bat phan la %o" %(sothapphan, sothapphan))
