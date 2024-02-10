ce4f43c89c4f4960c7d83b4602ad6d641f848865
Bang <151416686+bunnyz8888@users.noreply.github.com>
2024-02-10 08:20:07 +0700

    Add files via upload

---
+++ 19.Quadratic_equations.py
@@ -0,0 +1,24 @@
+#Giải phương trình bậc hai
+import math
+print ('Nhap 3 he so cach nhau boi dau cach')
+a, b, c = map(float, input().split())
+
+if a == 0:
+    if b == 0:
+        if c == 0:
+            print ('Phuong trinh co vo so nghiem')
+        else:
+            print ('Phuong trinh vo nghiem')
+    else:
+        print ('Phuong trinh co 1 nghiem duy nhat: \nx = {}'.format(-c/b))
+else:
+    delta = b*b - 4*a*c
+    if delta > 0:
+       x1 = float((-b + math.sqrt(delta)) / (2 * a))
+       x2 = float((-b - math.sqrt(delta)) / (2 * a))
+       print ("Phuong trinh co hai nghiem phan biet la: \nx1 = {} \nx2 = {}".format(x1, x2))
+    elif delta == 0:
+       x = -b / (2 * a)
+       print("Phuong trinh co nghiem kep: \nx1 = x2 = {}".format(x))
+    else:
+       print("Phuong trinh vo nghiem")
