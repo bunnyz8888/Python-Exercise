a289b33bda784aa35d6614335cc84ece8ed33187
Bang <151416686+bunnyz8888@users.noreply.github.com>
2024-02-15 09:50:32 +0700

    Add files via upload

---
+++ 24.Print_number_separate_line.py
@@ -0,0 +1,8 @@
+#Hiển thị 10 số tự nhiên đầu tiên ra màn hình trên từng dòng
+#Cau truc lap for
+for i in range(10):
+   #Cac lenh trong vong for duoc thuc hien nhieu lan
+   print(i)
+#Menh de else duoc thuc hien khi vong lap ket thuc binh thuong, khong bi break
+else:
+   print("Done!")
